49c2e84b76c8627c4ff656d25d817bc04aa96c0c
NadasyB <nadasy.bendeguz@gmail.com>
2021-03-19 12:23:31 +0100

    CSS elkészítve, minimál dizájn készítve

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Városok | Nádasy Bendegúz</title>
-        
+        <link rel="icon" href="varos.png" type="image/png" sizes="16x16">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js"></script>
         
@@ -12,11 +12,22 @@
         <link href="stilus.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <form>
-            <label>Kereső</label>
-            <input type="text" id="kermezo">
-                
-        </form>
-        <div id="tart"></div>
+        <main>
+            <header>
+                <h1>Magyar városok</h1>
+            </header>
+            <article>
+                <h3>Város keresése:</h3>
+                <form>
+                    <label>Kereső:</label>
+                    <input type="text" id="kermezo">
+                </form>
+                <div id="tart"></div>
+            </article>
+            <footer>
+                <p>Készítette: Nádasy Bendegúz</p>
+            </footer>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </main>
     </body>
 </html>
